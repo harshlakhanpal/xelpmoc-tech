af0685ba4b391031d7f1e1aa0e152d0afb308940
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-08-06 15:06:39 +0530

    Remove extra code

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import Login from "./components/Login";
 import Main from "./components/Main";
 import Register from "./components/Register";
 import Feed from "./components/Feed";
-// import User from "./components/User";
-// import Drawer from "./components/Drawer";
+
 import PrivateRoute from "./components/PrivateRoute";
 import Header from "./components/Header";
 import { Switch, Route } from "react-router-dom";
@@ -14,7 +13,6 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="app">
-      {/* <Drawer /> */}
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Switch>
         <div className="content">
